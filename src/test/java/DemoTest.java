8bbf6488c7311d490351f2da26dc256a09558363
vrokoshanskii <vrokoshanskiy@gmail.com>
2018-07-29 20:21:30 +0300

    Initial commit

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,35 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public static void init() {
+        ChromeDriverManager.getInstance().setup();
+    }
+
+    @Test(enabled = false)
+    public void testGoogleSearch() throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
